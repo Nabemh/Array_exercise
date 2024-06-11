de389bcdbfa464443e4ab4f25648afbb0cd63cb1
Nabemh <nhanior@gmail.com>
2024-06-11 23:28:14 +0100

    post-submission considerations

--- Array_rot.java
+++ Array_rot.java
@@ -1,14 +1,14 @@
-/**
- * rotLeft
- */
 public class Array_rot {
 
     public static int[] rotLeft(int[] a, int d){
         int n_elements = a.length;
         int[] rot = new int[n_elements];
-        int i;
 
-        for (i = 0; i < n_elements; i++){
+        if (d == 0 || d == a.length) {
+            return a;
+        }
+
+        for (int i = 0; i < n_elements; i++){
             int index = (i + (n_elements - d)) % n_elements;
             rot[index] = a[i];
         }
--- Main.java
+++ Main.java
@@ -2,7 +2,16 @@ public class Main {
     public static void main(String[] args) {
         int[] a = {5,4,3,2,1};
         int d = 4;
+
+        System.out.print("Original array: ");
+        for (int n = 0; n < a.length; n++) {
+            System.out.print(a[n]+ " ");
+        }
+
         int[] rot = Array_rot.rotLeft(a, d);
+
+        System.out.printf("\nArray after %d rotation(s): ", d);
+
         for (int i = 0; i < rot.length; i++) {
             System.out.print(rot[i] + " ");
         }
